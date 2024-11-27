c59a712eb2da7693b2f1e2a1287367e4df3b7ce3
Alexandre <4lexcode@gmail.com>
2024-11-27 11:03:16 -0300

    Background do main

---
+++ js/burguer.js
@@ -0,0 +1,5 @@
+// Código JavaScript para alternar o menu
+document.getElementById('menu-toggle').addEventListener('click', function() {
+    var navLinks = document.getElementById('nav-links');
+    navLinks.classList.toggle('show');
+});
